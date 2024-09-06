a7180f0b87b3a55723f97e511f1433b1157e92af
yashgaur-teblox <yash.gaur@teblox.com>
2024-09-06 12:48:04 +0530

    fix: hashrouter added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './App.css'
 import ThemeProvider from './components/ThemeProvider'
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
-  
+
   return (
     <ThemeProvider>
       
