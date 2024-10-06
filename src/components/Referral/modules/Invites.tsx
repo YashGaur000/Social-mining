ccfcf41dbe3dc12352b4df4bf719cedb6c80df38
mandeep TeBlox <mandeep.kumar@teblox.com>
2024-10-06 13:53:16 +0530

    setup eslint & husky

--- src/components/Referral/modules/Invites.tsx
+++ src/components/Referral/modules/Invites.tsx
@@ -1,24 +1,15 @@
-import React, { useState } from "react";
-import { 
-  CardContainer, 
-  InviteWrapper 
-} from "../styles/Invites.style";
-import { 
-  Card, 
-  CardBox, 
-  CardWrapper 
-} from "../../common/Card";
-import { 
-  LetsGoButton, 
-  Score, 
-  SocialLogo, 
-  SocialLogoBox, 
-  TitleBox 
-} from "../../TaskList/styles/TaskList.style";
-import { 
-  GlobalButton 
-} from "../../common/Buttons/GlobalButton";
-import { 
+import React, { useState } from 'react';
+import { CardContainer, InviteWrapper } from '../styles/Invites.style';
+import { Card, CardBox, CardWrapper } from '../../common/Card';
+import {
+  LetsGoButton,
+  Score,
+  SocialLogo,
+  SocialLogoBox,
+  TitleBox,
+} from '../../TaskList/styles/TaskList.style';
+import { GlobalButton } from '../../common/Buttons/GlobalButton';
+import {
   EarnedPoints,
   Name,
   ParticipantBox,
@@ -28,53 +19,56 @@ import {
   RankandPointBoxWrapper,
   RankandPointValueBox,
   RankBox,
-  Rankers 
-} from "../../Leaderboard/styles/Leaderboard.style";
+  Rankers,
+} from '../../Leaderboard/styles/Leaderboard.style';
 import ReferralSideDesign from '../../../assets/referralsidedesign.svg';
-import Bigtenex from "../../../assets/bigtenexbg.svg";
-import { MiddleLogo } from "../../DashBoard/styles/DashBoard.styles";
-import tenexbglogo from "../../../assets/tenexbglogo.svg";
-import tenexbglogo2 from "../../../assets/tenexbglogo2.svg";
-import axios from "axios";
+import Bigtenex from '../../../assets/bigtenexbg.svg';
+import { MiddleLogo } from '../../DashBoard/styles/DashBoard.styles';
+import tenexbglogo from '../../../assets/tenexbglogo.svg';
+import tenexbglogo2 from '../../../assets/tenexbglogo2.svg';
+import axios from 'axios';
 // import axios from "axios";
 
 const Invite: React.FC = () => {
-
   const [isCopied, setIsCopied] = useState(false);
+  console.log(setIsCopied);
 
   const players = [
-    "Tenex",
-    "Tenex",
-    "Tenex",
-    "Tenex",
-    "Tenex",
-    "Tenex",
-    "Tenex",
-    "Tenex",
-    "Tenex",
-    "Tenex",
-    "Tenex",
+    'Tenex',
+    'Tenex',
+    'Tenex',
+    'Tenex',
+    'Tenex',
+    'Tenex',
+    'Tenex',
+    'Tenex',
+    'Tenex',
+    'Tenex',
+    'Tenex',
   ];
 
   const playersPoints = [
-    "34523",
-    "34543",
-    "23455",
-    "45644",
-    "34534",
-    "34523",
-    "34543",
-    "23455",
-    "45644",
-    "34534",
-    "64544",
+    '34523',
+    '34543',
+    '23455',
+    '45644',
+    '34534',
+    '34523',
+    '34543',
+    '23455',
+    '45644',
+    '34534',
+    '64544',
   ];
-  
-  const handleGenerateReferralLink = async() => {
-    try {
-      const discordId = localStorage.getItem("userId");
 
-      const response = await axios.post('http://localhost:3000/api/users/referral',{discordId });
+  const handleGenerateReferralLink = async () => {
+    try {
+      const discordId = localStorage.getItem('userId');
+
+      const response = await axios.post(
+        'http://localhost:3000/api/users/referral',
+        { discordId }
+      );
       console.log(response.data.email);
     } catch (error) {
       console.log(error);
@@ -83,12 +77,12 @@ const Invite: React.FC = () => {
 
   // const copyToClipboard = async () => {
   //   const link = import.meta.env.VITE_REFERRAL;
-    
+
   //   if (!link) {
   //     console.error('Referral link is not defined');
   //     return;
   //   }
-    
+
   //   try {
   //     await navigator.clipboard.writeText(link);
   //     setIsCopied(true);
@@ -100,22 +94,30 @@ const Invite: React.FC = () => {
   //   }
   // };
 
-  
-
   return (
     <InviteWrapper>
-       
-  <MiddleLogo src={tenexbglogo} Top="53vh" Left="40vh" />
-  <MiddleLogo src={tenexbglogo2} Top="65vh" Left="150vh" />
+      <MiddleLogo src={tenexbglogo} Top="53vh" Left="40vh" />
+      <MiddleLogo src={tenexbglogo2} Top="65vh" Left="150vh" />
       <CardContainer margin="100px 0px 100px 0px">
         <CardWrapper>
           <SocialLogoBox>
-            <SocialLogo margin="0px -30px 0px 0px" height="100px" src={ReferralSideDesign} alt="Side Design" Zindex="2" />
+            <SocialLogo
+              margin="0px -30px 0px 0px"
+              height="100px"
+              src={ReferralSideDesign}
+              alt="Side Design"
+              Zindex="2"
+            />
           </SocialLogoBox>
           <CardBox>
             <TitleBox>
-              <Score Fontsize="24px" Lineheight="35.88px" Margin="0px 0px 0px 50px">
-                When they join, both you and your friend will receive 10 points each!
+              <Score
+                Fontsize="24px"
+                Lineheight="35.88px"
+                Margin="0px 0px 0px 50px"
+              >
+                When they join, both you and your friend will receive 10 points
+                each!
               </Score>
               <LetsGoButton margin="8px 50px 0px 800px">
                 <GlobalButton onClick={handleGenerateReferralLink}>
@@ -125,7 +127,11 @@ const Invite: React.FC = () => {
                   connect
                 </GlobalButton> */}
               </LetsGoButton>
-              {isCopied && <Score Margin="10px 0 0 50px" color="green">Link copied to clipboard!</Score>}
+              {isCopied && (
+                <Score Margin="10px 0 0 50px" color="green">
+                  Link copied to clipboard!
+                </Score>
+              )}
             </TitleBox>
           </CardBox>
         </CardWrapper>
@@ -133,44 +139,44 @@ const Invite: React.FC = () => {
       <CardContainer margin="0px 0px 0px 0px">
         {/* <RankandPointFullBoxWrapper> */}
         <SocialLogo
-    src={Bigtenex}
-    Position="absolute"
-    margin="15px 0px 0px -10px"
-    Zindex="1"
-  />
-          <Card
-            width="40%"
-            background="linear-gradient(180deg, rgba(24, 38, 76, .2) 0%, rgba(31, 48, 95, .4) 100%)"
-            Borderradius="24px"
-            Padding="30px 50px 40px 50px"
-          >
-            <RankandPointBoxWrapper>
-              <RankandPointBox>
-                <RankBox>
-                  <Rankers Fontsize="24px">List of your friends (20)</Rankers>
-                </RankBox>
-                <PointBox>
-                  <Points Fontsize="24px">Earned Points</Points>
-                </PointBox>
-              </RankandPointBox>
+          src={Bigtenex}
+          Position="absolute"
+          margin="15px 0px 0px -10px"
+          Zindex="1"
+        />
+        <Card
+          width="40%"
+          background="linear-gradient(180deg, rgba(24, 38, 76, .2) 0%, rgba(31, 48, 95, .4) 100%)"
+          Borderradius="24px"
+          Padding="30px 50px 40px 50px"
+        >
+          <RankandPointBoxWrapper>
+            <RankandPointBox>
+              <RankBox>
+                <Rankers Fontsize="24px">List of your friends (20)</Rankers>
+              </RankBox>
+              <PointBox>
+                <Points Fontsize="24px">Earned Points</Points>
+              </PointBox>
+            </RankandPointBox>
 
-              <RankandPointValueBox>
-                {players.map((player, index) => (
-                  <ParticipantBox key={index}>
-                    <RankBox>
-                      <Score>{index + 1}</Score>
-                    </RankBox>
-                    <Name>
-                      <Score>{player}</Score>
-                    </Name>
-                    <EarnedPoints>
-                      <Score>{playersPoints[index]}</Score>
-                    </EarnedPoints>
-                  </ParticipantBox>
-                ))}
-              </RankandPointValueBox>
-            </RankandPointBoxWrapper>
-          </Card>
+            <RankandPointValueBox>
+              {players.map((player, index) => (
+                <ParticipantBox key={index}>
+                  <RankBox>
+                    <Score>{index + 1}</Score>
+                  </RankBox>
+                  <Name>
+                    <Score>{player}</Score>
+                  </Name>
+                  <EarnedPoints>
+                    <Score>{playersPoints[index]}</Score>
+                  </EarnedPoints>
+                </ParticipantBox>
+              ))}
+            </RankandPointValueBox>
+          </RankandPointBoxWrapper>
+        </Card>
         {/* </RankandPointFullBoxWrapper> */}
       </CardContainer>
       <Score Margin="20px 0px 0px 0px" textalign="center">
